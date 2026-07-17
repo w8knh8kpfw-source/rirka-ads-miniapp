5326f73f0f2cbf469d9f7c3b9177f4af5b06090c
w8knh8kpfw-source <w8knh8kpfw@privaterelay.appleid.com>
2026-07-17 13:51:18 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,69 @@
 const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
 
 app.use(express.json());
 
 
-app.post("/send", (req,res)=>{
+const TOKEN = process.env.BOT_TOKEN;
+const ADMIN = process.env.ADMIN_ID;
 
-console.log(req.body);
+
+app.post("/send", async (req,res)=>{
+
+
+const data = req.body;
+
+
+const message = `
+🆕 Новая заявка RIRKA Ads
+
+👤 Пользователь:
+${data.name || "Не указано"}
+
+📌 Проект:
+${data.project}
+
+📢 Описание:
+${data.description}
+
+🔗 Ссылка:
+${data.link}
+
+📝 Текст поста:
+${data.postText}
+
+⏳ Срок:
+${data.duration}
+`;
+
+
+
+await fetch(
+`https://api.telegram.org/bot${TOKEN}/sendMessage`,
+{
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+chat_id:ADMIN,
+
+text:message
+
+})
+
+});
+
 
 res.json({
 success:true
 });
+
 
 });
 
