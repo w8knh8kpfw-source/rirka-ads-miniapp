cefd24abb65c1bd4fe1452d4c2b798a878e64230
w8knh8kpfw-source <w8knh8kpfw@privaterelay.appleid.com>
2026-07-17 18:43:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
+const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
 
